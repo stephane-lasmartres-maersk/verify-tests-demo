3bbeb0f25769b0ee41b5e681f2eb30f63ed7b874
Stephane Lasmartres <stephane.lasmartres@maersk.com>
2022-01-07 14:58:23 +0100

    Added a test project with a basic unique test

---
+++ TryingVerify/TryingVerify/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace TryingVerify
+{
+    internal class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Budget { get; set; }
+        public List<string> Starring { get; set; }
+    }
+}
---
+++ TryingVerify/TryingVerify/Movies.cs
@@ -0,0 +1,41 @@
+﻿namespace TryingVerify
+{
+    internal class Movies
+    {
+        public static List<Movie> Get()
+            => new List<Movie>
+            {
+                new Movie
+                {
+                    Id = Guid.Parse("a5eaae6f-e0bc-4fd4-b120-8dedf32adeb5"),
+                    Title = "The Secret Life of Walter Mitty",
+                    ReleaseDate = new DateTime(2013, 10, 5),
+                    Budget = 90_000_000,
+                    Starring = new List<string>
+                    {
+                        "Ben Stiller",
+                        "Kristen Wiig",
+                        "Shirley MacLaine",
+                        "Adam Scott",
+                        "Kathryn Hahn",
+                        "Sean Penn"
+                    }
+                },
+                new Movie
+                {
+                    Id = Guid.Parse("91287e2d-ca7c-48c5-95d0-43ebcd77a817"),
+                    Title = "Star Wars: The Rise of Skywalker",
+                    ReleaseDate = new DateTime(2019, 12, 16),
+                    Budget = 275_000_000,
+                    Starring = new List<string>
+                    {
+                        "Carrie Fisher",
+                        "Mark Hamill",
+                        "Adam Driver",
+                        "Daisy Ridley",
+                        "John Boyega"
+                    }
+                }
+            };
+    }
+}
